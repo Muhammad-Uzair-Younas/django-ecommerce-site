9748d94411d6d8fcc929282a457fcf08d6b8f12b
Muhammad-Uzair-Younas <muhammaduzairyounas88@gmail.com>
2026-05-15 15:52:27 +0500

    Update settings.py

--- brand/settings.py
+++ brand/settings.py
@@ -116,9 +116,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 import os
-
 # for storing media
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-MEDIA_URLS = '/media/'
+MEDIA_URL = '/media/'
